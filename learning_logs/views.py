111decac3ad399012eb66f1a283c0c51aa5f7211
KostiantynVotinov <votinovkostiantynwork@gmail.com>
2023-06-21 15:08:03 +0300

    Fixing 404, 500 error pages.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -21,8 +21,8 @@ def topics(request):
 
 @login_required
 def topic(request, topic_id):
-    """Показати одну тему та всі записи в ній."""
-    topic = Topic.objects.get(id=topic_id)
+    """Показати одну тему та всі записи до неї."""
+    topic = get_object_or_404(Topic, id=topic_id)
     # Пересвідчетися, що тема належить поточному користувачеві.
     if topic.owner != request.user:
         raise Http404
